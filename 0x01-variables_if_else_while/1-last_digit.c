e47461adcde34d4801464fceec63f22f5d0410cd
Kinprog <kingofori19@gmail.com>
2023-06-16 21:19:26 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	int n, lastd;
 
 	srand(time(0));
-	n == rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
 
-	if (listd > 5)
+	if (lastd > 5)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
