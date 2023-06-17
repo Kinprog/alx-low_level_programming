b47a505c187d1b78d53cca0661d4ce457917bfd3
Kinprog <kingofori19@gmail.com>
2023-06-17 06:42:07 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,16 +4,14 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	int d;
 	char low;
 
 	for (d = '0'; d <= '9'; d++)
 	putchar(d);
-	
+
 	for (low = 'a'; low <= 'f'; low++)
 	putchar(low);
 	putchar('\n');
